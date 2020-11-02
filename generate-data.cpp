38ab050437b4f93c4a73ff8162c436fb24b12c55
augenzu <elfokd@gmail.com>
2020-11-03 02:29:21 +0300

    Add random data generator

---
+++ generate-data.cpp
@@ -0,0 +1,15 @@
+#include "generate-data.h"
+
+int *
+test_data(size_t elm_cnt)
+{
+    int *data = new int[elm_cnt];
+
+    std::srand(42);
+
+    for (size_t i = 0; i < elm_cnt; ++i) {
+        data[i] = std::rand();
+    }
+
+    return data;
+}
---
+++ generate-data.h
@@ -0,0 +1,10 @@
+#ifndef GENERATE_DATA_H
+#define GENERATE_DATA_H
+
+#include <iostream>
+#include <cstdlib>
+
+int *
+test_data(size_t);
+
+#endif
