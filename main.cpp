3a647195a18051afe3906203a6ee413ef775512f
augenzu <elfokd@gmail.com>
2020-11-13 02:59:38 +0300

    New params

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 int
 main()
 {
-    std::vector<size_t> nums_threads{ 1, 2, 4, 8 };
-    std::vector<size_t> elm_cnts{ 100, 1000 };
+    std::vector<size_t> nums_threads{ 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 };
+    std::vector<size_t> elm_cnts{ 1000, 10000, 100000, 1000000, 10000000, 100000000,
+                                  1000, 10000, 100000, 1000000, 10000000, 100000000,
+                                  1000, 10000, 100000, 1000000, 10000000, 100000000,
+                                  1000, 10000, 100000, 1000000, 10000000, 100000000
+                                };
     std::string fname = "timings/12-11-20.txt";
 
     time_testing(nums_threads, elm_cnts, fname);
