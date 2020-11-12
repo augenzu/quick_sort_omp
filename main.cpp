010ce5418adb941eeec4b881ca373f2fb29cf672
augenzu <elfokd@gmail.com>
2020-11-13 00:08:00 +0300

    Add saving results into file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 int
 main()
 {
-    testing();
+    std::vector<size_t> nums_threads{ 1, 2, 4, 8 };
+    std::vector<size_t> elm_cnts{ 100, 1000 };
+    std::string fname = "timings/12-11-20.txt";
+
+    time_testing(nums_threads, elm_cnts, fname);
     
     return 0;
 }
--- test.h
+++ test.h
@@ -5,19 +5,21 @@
 #include "qsort-omp.h"
 #include <cstdio>
 #include <ctime>
+#include <fstream>
 #include <functional>
 #include <iostream>
 #include <vector>
 
 void
+time_testing(const std::vector<size_t> &, const std::vector<size_t> &,
+        const std::string &);
+
+//------------------------------------------------------------------
+
+
+void
 print(int *, size_t);
 void
 testing();
-// I'm not sure that's exactly I want to have here
-// maybe we should use smth like std::pair or std::tuple instead of vecotrs...
-// maybe vectors of necessary (what exactly?) size can't exist at all and hence we have to read data from files...
-// I dunno...
-std::vector<std::vector<double>>
-time_testing(const std::vector<size_t> &, const std::vector<size_t> &);
 
 #endif
