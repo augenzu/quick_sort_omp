b8aaf47b286bed932ecf0954afed16a43f632372
augenzu <elfokd@gmail.com>
2020-11-14 00:16:53 +0300

    Small fixes

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ main(int argc, char **argv)
     clock_t start = clock();
     q_sort(data, 1000000);
     clock_t end = clock();
+
+    delete[] data;
     double elapsed = static_cast<double>(end - start) / CLOCKS_PER_SEC;
   
     std::string fname = std::string("timings/") + argv[1] + ".txt";
