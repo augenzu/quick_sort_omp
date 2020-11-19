5e6c45a42df3268256dbaa8f754457ad7b4427e2
augenzu <elfokd@gmail.com>
2020-11-20 02:07:47 +0300

    Add run-tests script

---
+++ run-tasks.py
@@ -0,0 +1,31 @@
+import os
+
+
+pattern = \
+'''
+#BSUB -W 00:01
+#BSUB -o bsub-report.out
+#BSUB -e bsub-report.err
+#BSUB -R "span[hosts=1]"
+'''
+
+cmd = 'bsub < task.lsf'
+
+
+for num_procs in [1, 2, 4, 6, 8, 10, 12, 16]:
+    nsteps = 10
+    start = ((num_procs + 3) // 4) * 4
+    end = start * 16
+    step = (end - start) // nsteps
+    nums_threads = list(range(start, end + 1, step))
+    if num_procs not in nums_threads:
+        nums_threads = [num_procs] + nums_threads
+    else:
+        nums_threads += [nums_threads[-1]]
+    for num_threads in nums_threads:
+        task = '#BSUB -n ' + str(num_procs) + pattern + \
+                'OMP_NUM_THREADS=' + str(num_threads) + \
+                ' ./main 19-11-20__1 ' + str(num_procs)
+        with open('task.lsf', 'w') as fout:
+            fout.write(task)
+        os.system(cmd)
